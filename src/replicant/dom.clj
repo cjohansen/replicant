dea9d2fa79548035c4b7472285aa1f106a694d6a
Christian Johansen <christian@cjohansen.no>
2025-01-04 23:14:50 +0100

    Mimic dom.cljs in dom.clj to avoid displaying them differently on cljdoc

--- src/replicant/dom.clj
+++ src/replicant/dom.clj
@@ -2,8 +2,18 @@
 
 ;; This namespace makes it possible to use these functions in cljc files.
 
-(defn ^:export render [_el _hiccup & [_opt]])
+(defn ^:export render
+  "Render `hiccup` in DOM element `el`. Replaces any pre-existing content not
+  created by this function. Subsequent calls with the same `el` will update the
+  rendered DOM by comparing `hiccup` to the previous `hiccup`."
+  [el hiccup & [{:keys [aliases alias-data]}]])
 
-(defn ^:export unmount [_el])
+(defn ^:export unmount
+  "Unmounts elements in `el`, and clears internal state."
+  [el])
 
-(defn ^:export set-dispatch! [_f])
+(defn ^:export set-dispatch!
+  "Register a global dispatch function for event handlers and life-cycle hooks
+  that are not functions. See data-driven event handlers and life-cycle hooks in
+  the user guide for details."
+  [f])
